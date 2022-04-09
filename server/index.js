b6a5e27695c0213dfb98d5df1fd20a8d105d2f3d
Natasha Goryanina <84124317+natashagoryanina@users.noreply.github.com>
2022-04-09 22:49:02 +0300

    socket

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on("connection", (socket) => {
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit("receive_message", data);
+    });
+    socket.on("send_ticker", (data) => {
+        socket.broadcast.emit("receive_ticker", data);
+    });
+});
+
+server.listen(4000, () => {
+    console.log("Server is running");
+});
